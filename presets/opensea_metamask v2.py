56caf9a4295a373cc58dc8ced943010ca34c6aa4
Ali Raza <aliatwork@hotmail.co.uk>
2021-11-23 16:54:58 +0000

    updated opensea metamask preset

--- presets/opensea_metamask v2.py
+++ presets/opensea_metamask v2.py
@@ -7,6 +7,7 @@ loop_title = "#"
 loop_file_format = "png"
 loop_external_link = "https://linktr.ee/Cloudmaking"
 loop_description = "Lone wanderer is a series of 10000 collectible planets. The Rarity of the planets increases as we reach out max storage limit of 10000."
+Collection_type = "polygon" #pick "polygon" or "ethereum"
 #####VARIABLES END########
 
 while loop_amount != 0:
@@ -57,27 +58,50 @@ while loop_amount != 0:
 	###LISTING####
 	current_page=driver.current_url
 	main_page = driver.current_window_handle
-	while True:
+
+	if Collection_type == "polygon":
+		while True:
+			try:
+				linktext_click("Sell")
+				css_and_key("input[placeholder='Amount']", str(loop_price))
+				css_and_click("button[type='submit']")
+				css_and_click("button[class='Blockreact__Block-sc-1xf18x6-0 Buttonreact__StyledButton-sc-glfma3-0 bhqEJb kCijbX']")
+				time.sleep(2)
+				for handle in driver.window_handles:
+					if handle != main_page:
+						login_page = handle
+				driver.switch_to.window(login_page)
+				css_and_click("button[data-testid='request-signature__sign']")
+				time.sleep(1)
+				driver.switch_to.window(main_page)
+				css_and_click("i[aria-label='Close']")
+				break
+			except Exception as wtf:
+				#print(wtf)
+				print("----- refreshing and retrying -----")
+				go_to(current_page)
+				continue
+	elif Collection_type == "ethereum":
+		while True:
 		try:
 			linktext_click("Sell")
 			css_and_key("input[placeholder='Amount']", str(loop_price))
 			css_and_click("button[type='submit']")
-			css_and_click("button[class='Blockreact__Block-sc-1xf18x6-0 Buttonreact__StyledButton-sc-glfma3-0 bhqEJb kCijbX']")
-			time.sleep(2)
-			for handle in driver.window_handles:
-				if handle != main_page:
-					login_page = handle
-			driver.switch_to.window(login_page)
-			css_and_click("button[data-testid='request-signature__sign']")
+			time.sleep(15) #change this depending on how quickly the metamask notification comes up
+			driver.switch_to.window(driver.window_handles[1])
+			xpath_and_click("//*[@id='app-content']/div/div[2]/div/div[3]/button[2]")
 			time.sleep(1)
 			driver.switch_to.window(main_page)
-			css_and_click("i[aria-label='Close']")
+			time.sleep(1)
 			break
 		except Exception as wtf:
-			#print(wtf)
-			print("----- refreshing and retrying -----")
+			print(wtf)
+			print("----- refreshing and trying -----")
 			go_to(current_page)
 			continue
+	else:
+		print("Please pick either 'polygon' or 'ethereum' / no caps, make sure the spellign is correct")
+		break
 		
 	start_num = start_num + 1
 	loop_amount = loop_amount - 1
